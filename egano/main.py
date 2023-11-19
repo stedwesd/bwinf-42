4df3e5e6c0a38eea2f1836361fa354185e76f139
stedwesd <72816557+stedwesd@users.noreply.github.com>
2023-11-19 15:50:49 +0100

    made egano's output compactible to other tasks
     - outputs result files to a folder instead of printing them out

--- egano/main.py
+++ egano/main.py
@@ -1,19 +1,20 @@
 from PIL import Image
 
-def getSecretMessage(path: str):
-    img = Image.open(path)
+def getSecretMessage(filename: str, output: str):
+    img = Image.open(filename)
     x, y, dx, dy = 0, 0, 0, 0
     w, h = img.size
     msg = ""
-    while not msg or dx or dy:
-        char, dx, dy = img.getpixel((x, y))[:3]
+    while not msg or dx or dy: # if we returned to the start and the message is not empty.
+        char, dx, dy = img.getpixel((x, y))[:3] # we are not interested in the alpha channel
         msg += chr(char) # add the characters
         x, y = x + dx, y + dy # add the offsets
         x, y = x % w, y % h # wrap over the ends of the picture
-    return msg
+    
+    with open(output, "w") as f:
+        f.write(msg)
 
 
 if __name__ == "__main__":
     for i in range(7):
-        print(f"\n\nIMAGE: bild0{i+1}.png\n\n")
-        print(getSecretMessage(f"egano/files/bild0{i+1}.png"))
+        getSecretMessage(f"files/bild0{i+1}.png", f"output/bild0{i+1}.txt")
